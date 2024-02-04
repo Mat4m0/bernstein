4efe9f0076a759c60e652371b7b30a2074883733
Mat4m0 <matthias.amon@me.com>
2024-02-04 13:38:06 +0100

    working

--- src/new.ts
+++ src/new.ts
@@ -5,68 +5,78 @@ import { CanvasData, AllCanvasNodeData, CanvasFileData, CanvasGroupData, CanvasN
 import { v4 as uuidv4 } from 'uuid';
 
 interface CanvasType {
-  path: string;
-  isSubCanvas: boolean;
-  canvasData: CanvasData;
-  canvasName: string;
-  readFile: () => Promise<void>; // Update type signature to return a Promise
+	path: string;
+	isSubCanvas: boolean;
+	canvasData: CanvasData;
+	canvasName: string;
+	readFile: () => Promise<void>; // Update type signature to return a Promise
 }
 
 export default async function startProcess() {
-	// Note: Handling of async creation and initialization of Canvas instances may need adjustment
-	new Canvas('/Users/matthias/Git/_Loopinum/bernstein/Bernstein Vault/', '5. DOCS/BEESPACE/Entry.canvas', false, "");
-  }
-  
+	const combinedData: CanvasData = { nodes: [], edges: [] }
+	const entryCanvas = new Canvas('/Users/matthias/Git/_Loopinum/bernstein/Bernstein Vault/', '5. DOCS/BEESPACE/Entry.canvas', false, "", combinedData);
+	await entryCanvas.initializeCanvas()
+	console.log(combinedData)
+	combinedData.nodes = await removeSubCanvase(combinedData.nodes)
+	await cleanupChildren(combinedData.nodes)
+	const outputFilePath = '/Users/matthias/Git/_Loopinum/bernstein/combinedData.json'; // Specify the output file path
+	await writeCombinedDataToDisk(combinedData, outputFilePath);
+}
+
 
 class Canvas implements CanvasType {
-  vaultPath: string;
-  canvasPath: string;
-  isSubCanvas: boolean;
-  canvasData: CanvasData;
-  canvasName: string;
-  nodeId: string;
+	vaultPath: string;
+	canvasPath: string;
+	isSubCanvas: boolean;
+	canvasData: CanvasData;
+	canvasName: string;
+	nodeId: string;
+	combinedData: CanvasData;
 
-  constructor(vaultPath: string, canvasPath: string, isSubCanvas: boolean, nodeId: string) {
-    this.canvasPath = canvasPath;
-    this.vaultPath = vaultPath;
-    this.isSubCanvas = isSubCanvas;
-    this.canvasData = {} as CanvasData;
-	this.canvasName = canvasPath.split("/").pop() as string
-	this.nodeId = nodeId
-    // Since constructors can't be async, move async initialization to a separate method
-    this.initializeCanvas();
-  }
-
-  async initializeCanvas() {
-    try {
-      await this.readFile();
-      this.checkForSubCanvas();
-      this.subCanvasProcessing();
-	  this.aggregateData();
-    } catch (error) {
-      console.error('Failed to initialize canvas:', error);
-    }
-  }
-
-  async readFile() {
-    try {
-      const canvasFile = await fs.readFile(path.join(this.vaultPath, this.canvasPath), 'utf8');
-      this.canvasData = JSON.parse(canvasFile);
-    } catch (error) {
-      console.error('Error reading canvas file:', error);
-      throw error; // Rethrow to allow the caller to handle it
-    }
-  }
-
-	checkForSubCanvas() {
-		this.canvasData.nodes
-			.filter((node: AllCanvasNodeData) => node.type === 'file' && node.file && node.file.endsWith('.canvas'))
-			.forEach((subCanvasNode: CanvasFileData) => {
-				const subCanvas: CanvasType = new Canvas(this.vaultPath, subCanvasNode.file, true, subCanvasNode.id);
-			})
+	constructor(vaultPath: string, canvasPath: string, isSubCanvas: boolean, nodeId: string, combinedData: CanvasData) {
+		this.canvasPath = canvasPath;
+		this.vaultPath = vaultPath;
+		this.isSubCanvas = isSubCanvas;
+		this.canvasData = {} as CanvasData;
+		this.canvasName = canvasPath.split("/").pop() as string
+		this.nodeId = nodeId
+		this.combinedData = combinedData;
 	}
 
-	subCanvasProcessing() {
+	async initializeCanvas() {
+		try {
+			await this.readFile();
+			await this.checkForSubCanvas();
+			await this.subCanvasProcessing();
+			this.aggregateData();
+		} catch (error) {
+			console.error('Failed to initialize canvas:', error);
+		}
+	}
+
+	async readFile() {
+		try {
+			const canvasFile = await fs.readFile(path.join(this.vaultPath, this.canvasPath), 'utf8');
+			this.canvasData = JSON.parse(canvasFile);
+			addChildrenToGroups(this.canvasData.nodes)
+		} catch (error) {
+			console.error('Error reading canvas file:', error);
+			throw error; // Rethrow to allow the caller to handle it
+		}
+	}
+
+	async checkForSubCanvas() {
+		await Promise.all(
+			this.canvasData.nodes
+				.filter((node: AllCanvasNodeData) => node.type === 'file' && node.file && node.file.endsWith('.canvas'))
+				.map(async (subCanvasNode: CanvasFileData) => {
+					const subCanvas: CanvasType = new Canvas(this.vaultPath, subCanvasNode.file, true, subCanvasNode.id, this.combinedData);
+					await subCanvas.initializeCanvas();
+				})
+		);
+	}
+
+	async subCanvasProcessing() {
 		if (this.isSubCanvas == true) {
 			addNewGroup(this.canvasData, this.canvasName, this.nodeId)
 
@@ -74,33 +84,54 @@ class Canvas implements CanvasType {
 	}
 
 	aggregateData() {
-		
+		this.combinedData.nodes.push(...this.canvasData.nodes);
+		this.combinedData.edges.push(...this.canvasData.edges);
 	}
 
-	
+}
+
+async function cleanupChildren(groups: AllCanvasNodeData[]) {
+    groups.forEach((groupA) => {
+      const removeIds: Set<string> = new Set();
+      groupA.children.forEach((childId) => {
+        const childNode = groups.find((groupB) => groupB.id === childId);
+        if (childNode) {
+          childNode.children.forEach((grandChildId) => {
+            removeIds.add(grandChildId);
+          });
+        }
+      });
+      groupA.children = groupA.children.filter((childId: string) => !removeIds.has(childId));
+    });
+  }
+
+
+async function removeSubCanvase(combinedData: AllCanvasNodeData[]) {
+	return combinedData.filter((node) => !(node.type == 'file' && node.file.endsWith(".canvas")))
+
+
 }
 
 
-
 function addNewGroup(canvasData: CanvasData, canvasName: string, nodeId: string) {
-    // Generate a UUID for the new group
-    const newGroupId = generateCustomId();
+	// Generate a UUID for the new group
+	const newGroupId = generateCustomId();
 
-    // Get all node IDs to add as children to the new group
-    const allNodeIds = canvasData.nodes.map((node) => node.id);
+	// Get all node IDs to add as children to the new group
+	const allNodeIds = canvasData.nodes.map((node) => node.id);
 
-    // Create a new group node with all existing node IDs as children
-    const newGroup: CanvasGroupData = {
-        id: nodeId,
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0,
-        type: 'group',
-        label: canvasName,
-        children: allNodeIds,
-    };
-    canvasData.nodes.push(newGroup);
+	// Create a new group node with all existing node IDs as children
+	const newGroup: CanvasGroupData = {
+		id: nodeId,
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0,
+		type: 'group',
+		label: canvasName,
+		children: allNodeIds,
+	};
+	canvasData.nodes.push(newGroup);
 }
 
 function isContainedIn(groupA: AllCanvasNodeData, groupB: AllCanvasNodeData): boolean {
@@ -113,25 +144,37 @@ function isContainedIn(groupA: AllCanvasNodeData, groupB: AllCanvasNodeData): bo
 }
 
 function generateCustomId(length: number = 16): string {
-    let uuid = uuidv4().replace(/-/g, ''); // Remove hyphens
-    return uuid.substring(0, length); // Truncate to the desired length
+	let uuid = uuidv4().replace(/-/g, ''); // Remove hyphens
+	return uuid.substring(0, length); // Truncate to the desired length
 }
 
 function addChildrenToGroups(groups: AllCanvasNodeData[]): void {
-    groups.forEach((groupA) => {
-        // Skip processing for groups with x, y, width, and height set to zero
-        if (groupA.x === 0 && groupA.y === 0 && groupA.width === 0 && groupA.height === 0) {
-            //console.log(`Skipping group ${groupA.id} due to zero dimensions and position.`);
-            return; // Skip further processing for this group
-        }
+	groups.forEach((groupA) => {
+		// Skip processing for groups with x, y, width, and height set to zero
+		if (groupA.x === 0 && groupA.y === 0 && groupA.width === 0 && groupA.height === 0) {
+			//console.log(`Skipping group ${groupA.id} due to zero dimensions and position.`);
+			return; // Skip further processing for this group
+		}
 
-        groupA.children = groups
-            .filter((groupB) => groupB.id !== groupA.id && isContainedIn(groupB, groupA))
-            .map((groupB) => groupB.id);
-    });
+		groupA.children = groups
+			.filter((groupB) => groupB.id !== groupA.id && isContainedIn(groupB, groupA))
+			.map((groupB) => groupB.id);
+	});
 }
 
+async function writeCombinedDataToDisk(combinedData: CanvasData, outputFilePath: string): Promise<void> {
+	try {
+		// Convert combinedData to JSON format
+		const combinedDataJSON = JSON.stringify(combinedData, null, 2); // Use null and 2 for pretty formatting
 
+		// Write the JSON data to the file
+		await fs.writeFile(outputFilePath, combinedDataJSON, 'utf8');
+		console.log(`Combined data has been written to ${outputFilePath}`);
+	} catch (error) {
+		console.error('Error writing combinedData to disk:', error);
+		throw error; // Rethrow the error to handle it externally if needed
+	}
+}
 
 
 
